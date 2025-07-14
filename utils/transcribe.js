4ce5b67e99ebb23a5d71f3df43fc853090c9f3eb
Felipe Rocha <feliperochadf32@gmail.com>
2025-07-13 21:58:53 -0300

    Add files via upload

--- utils/transcribe.js
+++ utils/transcribe.js
@@ -1,1 +1,14 @@
 
+const { createOpenAI } = require("openai");
+
+const openai = createOpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+async function transcribeAudio(audioUrl) {
+    const transcription = await openai.audio.transcriptions.create({
+        file: await fetch(audioUrl).then(r => r.blob()),
+        model: "whisper-1"
+    });
+    return transcription.text;
+}
+
+module.exports = { transcribeAudio };
